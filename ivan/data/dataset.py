b6b359d89dea6abfd69eed66ec61a3546bba3183
AmpiroMax <patrat2015@yandex.ru>
2024-05-05 13:01:17 +0300

    Training was added

--- ivan/data/dataset.py
+++ ivan/data/dataset.py
@@ -4,6 +4,8 @@ from PIL import Image
 import torch
 import os
 
+from ivan.data.processing import create_mask_from_image
+
 
 class ElectroInterDataset(Dataset):
     def __init__(self, data_foulder_path: str) -> None:
@@ -16,14 +18,29 @@ class ElectroInterDataset(Dataset):
             image = Image.open(data_foulder_path + "/images/" + file_name)
             label = Image.open(data_foulder_path + "/labels/" + file_name)
             scale_file_name = file_name.split(".")[0] + ".txt"
-            scale = self._open_scale(data_foulder_path + "/scales/" + scale_file_name)
+            scale = self._open_scale(
+                data_foulder_path + "/scales/" + scale_file_name)
 
             self.data[i] = (image, label, scale)
 
         # ====================================================================
         # Preparing transforms
         # ====================================================================
-        self.transform = transforms.Compose([transforms.PILToTensor()])
+        self.image_to_tensor = transforms.Compose([
+            transforms.PILToTensor(),
+        ])
+        self.label_to_tensor = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+        self.transform = transforms.Compose([
+            # transforms.Resize((100, 1100)),
+
+            # transforms.Normalize(
+            #     mean=[0.485, 0.456, 0.406],
+            #     std=[0.229, 0.224, 0.225]
+            # )
+        ])
 
     def get_sample(self, idx) -> tuple:
         return self.data[idx]
@@ -35,8 +52,12 @@ class ElectroInterDataset(Dataset):
         return scale
 
     def __getitem__(self, idx):
-        image_tensor = self.transform(self.data[idx][0]).permute(2, 0, 1)[None, ...]
-        label_tensor = self.transform(self.data[idx][1]).permute(2, 0, 1)[None, ...]
+        image_tensor = self.transform(self.image_to_tensor(
+            self.data[idx][0]).type(torch.FloatTensor))
+
+        label_tensor = self.transform(self.label_to_tensor(
+            create_mask_from_image(self.data[idx][1])).type(torch.LongTensor)).squeeze()
+
         item = (image_tensor, label_tensor)
 
         return item
--- ivan/data/processing.py
+++ ivan/data/processing.py
@@ -1,28 +1,52 @@
+import torch
 import numpy as np
 from PIL import Image
 
 
-def filter_label(label: np.ndarray):
-    label_filtred = label
-    label_filtred = label_filtred // 130 * 255
-    label_filtred = label_filtred.reshape(-1, 3)
+def create_mask_from_image(image: Image) -> np.ndarray:
+    array = np.array(image)
+    h, w, d = array.shape
+    mask = np.zeros((h, w))
 
-    for i in range(len(label_filtred)):
-        if all(label_filtred[i] == [255, 0, 255]):
-            label_filtred[i] = [255, 0, 0]
+    for i in range(h):
+        for j in range(w):
+            pix = array[i, j]
+            if pix[3] < 250:  # Пустой
+                mask[i, j] = 0
+            elif pix[0] == pix[1] == pix[2] == 255:  # Белый
+                mask[i, j] = 1
+            elif pix[0] == pix[1] == pix[2] == 0:  # Черный
+                mask[i, j] = 2
+            elif 250 <= pix[0] <= 255:  # Красный
+                mask[i, j] = 3
+            elif 250 <= pix[1] <= 255:  # Зеленый
+                mask[i, j] = 4
+            elif 250 <= pix[2] <= 255:  # Синий
+                mask[i, j] = 5
 
-        if all(label_filtred[i] == [255, 255, 0]):
-            label_filtred[i] = [255, 0, 0]
-
-        if all(label_filtred[i] == [0, 255, 255]):
-            label_filtred[i] = [0, 255, 0]
-
-        if all(label_filtred[i] == [255, 255, 255]):
-            label_filtred[i] = [0, 0, 0]
-
-    label_filtred = label_filtred.reshape(label.shape)
-
-    return label_filtred
+    return mask
 
 
-# def torch2pil(tensor) -> Image:
+def create_image_from_mask(mask: np.array) -> Image:
+    h, w = mask.shape
+    image = np.zeros((h, w, 4), dtype=np.uint8)
+
+    for i in range(h):
+        for j in range(w):
+            pix_class = mask[i, j]
+            match pix_class:
+                case 0:
+                    image[i, j] = (0, 0, 0, 0)
+                case 1:
+                    image[i, j] = (255, 255, 255, 255)
+                case 2:
+                    image[i, j] = (0, 0, 0, 255)
+                case 3:
+                    image[i, j] = (255, 0, 0, 255)
+                case 4:
+                    image[i, j] = (0, 255, 0, 255)
+                case 5:
+                    image[i, j] = (0, 0, 255, 255)
+
+    image = Image.fromarray(image)
+    return image
